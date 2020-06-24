4cef8962295a6e9820690d3ba4bdc595220161fd
dnetochaves <dnetochaves@gmail.com>
2020-06-24 07:23:40 -0300

    Closes #1

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import list_clientes, new
+from .views import list_clientes, new, update, delete
 
 urlpatterns = [
    path('list_clientes', list_clientes, name="list_clientes"),
    path('new', new, name="new"),
+   path('update/<int:id>/', update, name="update"),
+   path('delete/<int:id>/', delete, name="delete"),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
 from .forms import PersonForm
 
@@ -8,9 +8,31 @@ def list_clientes(request):
     return render(request, 'clientes/list_clientes.html', {'persons': persons})
 
 def new(request):
-    form = PersonForm(request.POST, request.FILES, None)
+    form = PersonForm(request.POST or request.FILES or None)
 
     if form.is_valid():
         form.save()
         return redirect('list_clientes')
     return render(request, 'clientes/person_form.html', {'form': form})
+
+
+
+def update(request, id):
+    person = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or request.FILES or None, instance=person) 
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_clientes')
+    return render(request, 'clientes/person_form.html', {'form': form})
+
+
+def delete(request, id):
+    person = get_object_or_404(Person, pk=id)
+
+    if request.method == 'POST':
+        person.delete()
+        return redirect('list_clientes')
+
+    return render(request, 'clientes/person_delete_confirm.html', {'person': person})
+
