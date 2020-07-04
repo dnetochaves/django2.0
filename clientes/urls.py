21a0232067977ac58bb2b006a38797d3b626e58c
dnetochaves <dnetochaves@gmail.com>
2020-07-04 04:51:44 -0300

    Organizando CBV

--- clientes/urls.py
+++ clientes/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
    path('delete/<int:id>/', delete, name="delete"),
 
    #CBV
-   path('person_list', PersonList.as_view(), name="person_list"),
+   path('person-list', PersonList.as_view(), name="person-list"),
    path('person_detail/<int:pk>/', PersonDetailView.as_view(), name='person-detail'),
-   path('person-create', PersonCreate.as_view(), name="person_create"),
+   path('person-create', PersonCreate.as_view(), name="person-create"),
    path('person-update/<int:pk>/', PersonUpdate.as_view(), name='person-update'),
    path('person-delete/<int:pk>/', PersonDelete.as_view(), name='person-delete'),
 ]
